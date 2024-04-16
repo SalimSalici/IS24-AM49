4fee44615c578f8a9f8c4951cb41036c5a33de21
Matteo <scarlinom00@gmail.com>
2024-04-16 10:28:40 +0200

    added player and playerboard tests

---
+++ src/test/java/it/polimi/ingsw/am49/model/players/PlayerTest.java
@@ -0,0 +1,62 @@
+package it.polimi.ingsw.am49.model.players;
+
+import it.polimi.ingsw.am49.model.cards.objectives.ObjectiveCard;
+import it.polimi.ingsw.am49.model.cards.placeables.PlaceableCard;
+import it.polimi.ingsw.am49.model.cards.placeables.ResourceCard;
+import it.polimi.ingsw.am49.model.cards.placeables.StarterCard;
+import it.polimi.ingsw.am49.model.decks.DeckLoader;
+import it.polimi.ingsw.am49.model.enumerations.CornerPosition;
+import it.polimi.ingsw.am49.model.enumerations.RelativePosition;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void placeCard() throws Exception {
+        StarterCard start = DeckLoader.getInstance().getNewStarterCardById(81);
+        Player matteo = new Player("Matteo");
+        matteo.setStarterCard(start);
+        ResourceCard res1 = DeckLoader.getInstance().getNewResourceCardById(1);
+        BoardTile starterTile = matteo.getBoard().getStarterTile();
+        matteo.drawCard(res1);
+        matteo.placeCard(res1, starterTile.getRow(), starterTile.getCol(), CornerPosition.TOP_RIGHT);
+        BoardTile tile1 = starterTile.getNeighbourTile(RelativePosition.TOP_RIGHT);
+        PlaceableCard testCard = tile1.getCard();
+        Assertions.assertEquals(res1, testCard);
+    }
+
+    @Test
+    void calculateFinalPoints() throws Exception {
+        StarterCard start = DeckLoader.getInstance().getNewStarterCardById(81);
+        Player matteo = new Player("Matteo");
+        matteo.setStarterCard(start);
+        ResourceCard res1 = DeckLoader.getInstance().getNewResourceCardById(1);
+        ResourceCard res2 = DeckLoader.getInstance().getNewResourceCardById(2);
+        ResourceCard res3 = DeckLoader.getInstance().getNewResourceCardById(3);
+        BoardTile starterTile = matteo.getBoard().getStarterTile();
+        matteo.drawCard(res1);
+        matteo.placeCard(res1, starterTile.getRow(), starterTile.getCol(), CornerPosition.TOP_RIGHT);
+        BoardTile tile1 = starterTile.getNeighbourTile(RelativePosition.TOP_RIGHT);
+        matteo.drawCard(res2);
+        matteo.placeCard(res2, tile1.getRow(), tile1.getCol(), CornerPosition.TOP_RIGHT);
+        BoardTile tile2 = tile1.getNeighbourTile(RelativePosition.TOP_RIGHT);
+        matteo.drawCard(res3);
+        matteo.placeCard(res3, tile2.getRow(), tile2.getCol(), CornerPosition.TOP_RIGHT);
+        ObjectiveCard obj1 = DeckLoader.getInstance().getNewObjectiveCardById(87);
+        ObjectiveCard obj2 = DeckLoader.getInstance().getNewObjectiveCardById(95);
+        List<ObjectiveCard> objList = new ArrayList<>();
+        objList.add(obj1);
+        matteo.setPersonalObjective(obj2);
+        int obj = matteo.calculateFinalPoints(objList);
+        Assertions.assertEquals(2, obj);
+
+    }
+}
